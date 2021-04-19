7bf4ce5e96f70de39bacd4b64f600fe4168f4c19
Nikita <c1aymorefn@gmail.com>
2021-04-19 20:54:41 +0300

    Lesson6

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Snake
 {
@@ -11,7 +12,27 @@ namespace Snake
 
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
-        }
 
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+
+            Console.ReadKey();
+        }
     }
 }
